190b727c316665dce65982ca5bcee14d3d34c174
Jose Thomas <145546568+josethomas45@users.noreply.github.com>
2024-03-18 10:11:12 +0530

    Create studentdetail.sh

---
+++ studentdetail.sh
@@ -0,0 +1,18 @@
+echo "Enter your details" 
+echo "Enter your name:"
+read name
+echo "Enter your Semester:"
+read sem
+echo "Enter your Batch:"
+read batch
+echo "Enter your Roll No:"
+read roll
+echo "Enter your register number:"
+read reg
+echo "---------------------------" 
+echo "Enter your details" 
+echo "Name: $name"
+echo "Semester: $sem"
+echo "Batch: $batch"
+echo "Roll No: $roll"
+echo "Register Number: $reg"
